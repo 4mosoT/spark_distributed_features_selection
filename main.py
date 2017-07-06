5f90241fd4be3a6de40a712001044c8d490aa0a8
marco <marco.a.formoso@gmail.com>
2017-07-06 23:38:33 +0200

    Added function to use in everypartition. Working with Chi2

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 from pyspark.sql import SparkSession
 import sklearn.feature_selection as fs
-import sklearn.preprocessing as pre
 import numpy as np
 import sys
-
-
 
 # TODO: Parse arguments
 
 ss = SparkSession.builder.appName("hsplit").master("local[*]").getOrCreate()
 
 dataframe = ss.read.option("maxColumns", "30000").csv(sys.argv[1])
-for column in dataframe.columns[:22200]:
-    dataframe = dataframe.drop(column)
-input = dataframe.rdd
-print(len(dataframe.columns))
+dataframe2 = dataframe.select([c for c in dataframe.columns[22200:]])
+input = dataframe2.rdd
+print(len(dataframe2.columns))
 numParts = 8
 br_numParts = ss.sparkContext.broadcast(numParts)
 
 # First we flatMap and get index for each key, then we assign every index to a partition
-partitioned = input.map(lambda row: (row[len(row) - 1], row)).groupByKey()\
-    .flatMap(lambda key_iterable: enumerate(key_iterable[1]))\
+partitioned = input.map(lambda row: (row[len(row) - 1], row)).groupByKey() \
+    .flatMap(lambda key_iterable: enumerate(key_iterable[1])) \
     .map(lambda index_row: (index_row[0] % br_numParts.value, index_row[1]))
+
 
 def funcion(s):
     iterable = s[1]
     classes = []
     features = []
     for x in iterable:
-        classes.append(x[len(x)-1])
-        features.append(x[:len(x)-1])
-    classes = np.asarray(classes)
-    features = np.asarray(features)
-    #sel = fs.SelectKBest(fs.chi2).fit(features, classes)
-    return (classes.shape, features.shape)
+        classes.append(x[len(x) - 1])
+        features.append(x[:len(x) - 1])
+    classes = np.array(classes).astype(np.float)
+    features = np.array(features).astype(np.float)
+    sel = fs.SelectKBest(fs.chi2).fit(features, classes)
+    return sel.get_support(True)
+
 
 print(partitioned.groupByKey().map(funcion).take(10))
-
