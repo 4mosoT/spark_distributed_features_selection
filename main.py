4ef8dc1ac56e0538b17beabb39a6470d3025805c
marco <marco.a.formoso@gmail.com>
2017-07-06 21:08:51 +0200

    Ignore .idea

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ br_numParts = ss.sparkContext.broadcast(numParts)
 partitioned = input.map(lambda row: (row[len(row) - 1], row)).groupByKey()\
     .flatMap(lambda key_iterable: enumerate(key_iterable[1]))\
     .map(lambda index_row: (index_row[0] % br_numParts.value, index_row[1]))
+
 print(partitioned.countByKey())
